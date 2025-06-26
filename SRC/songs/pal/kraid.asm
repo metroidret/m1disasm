cf4b538043aaffc2b5cd231eb8eba57acf11b7fe
CharlesVanEeckhout <vaneeckhoutcharles@gmail.com>
2025-06-26 15:34:10 -0400

    pal bank 4,5,6

--- SRC/songs/pal/kraid.asm
+++ SRC/songs/pal/kraid.asm
@@ -26,106 +26,106 @@ SongKraidSQ1:
 SongKraidSQ2:
     SongRepeatSetup $4
         SongNoteLength $3 ;1/2 seconds
-        SongNote "E4"
-        SongNoteLength $2 ;1/4 seconds
-        SongNote "B3"
-        SongNoteLength $3 ;1/2 seconds
-        SongNote "A4"
-        SongNoteLength $2 ;1/4 seconds
-        SongNote "C4"
-        SongNoteLength $3 ;1/2 seconds
         SongNote "F#4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "C#4"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "B4"
         SongNoteLength $2 ;1/4 seconds
         SongNote "D4"
         SongNoteLength $3 ;1/2 seconds
-        SongNote "B3"
+        SongNote "G#4"
         SongNoteLength $2 ;1/4 seconds
-        SongNote "A#3"
+        SongNote "E4"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "C#4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "C4"
     SongRepeat
     SongRepeatSetup $2
         SongNoteLength $3 ;1/2 seconds
-        SongNote "G4"
-        SongNoteLength $2 ;1/4 seconds
-        SongNote "D4"
-        SongNoteLength $3 ;1/2 seconds
-        SongNote "E4"
-        SongNoteLength $2 ;1/4 seconds
-        SongNote "B3"
-        SongNoteLength $3 ;1/2 seconds
-        SongNote "F#4"
-        SongNoteLength $2 ;1/4 seconds
-        SongNote "D4"
-        SongNoteLength $3 ;1/2 seconds
         SongNote "A4"
         SongNoteLength $2 ;1/4 seconds
         SongNote "E4"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "F#4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "C#4"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "G#4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "E4"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "B4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "F#4"
     SongRepeat
     SongRepeatSetup $4
         SongNoteLength $1 ;1/8 seconds
-        SongNote "G4"
-        SongNote "B3"
-        SongNote "G4"
-        SongNote "B3"
-        SongNote "G4"
-        SongNote "B3"
-        SongNote "A#4"
-        SongNote "E4"
-        SongNote "A#4"
-        SongNote "E4"
-        SongNote "A#4"
-        SongNote "E4"
         SongNote "A4"
-        SongNote "C4"
+        SongNote "C#4"
         SongNote "A4"
-        SongNote "C4"
+        SongNote "C#4"
         SongNote "A4"
-        SongNote "C4"
-        SongNote "A4"
-        SongNote "D#4"
+        SongNote "C#4"
+        SongNote "C5"
         SongNote "F#4"
-        SongNote "D#4"
+        SongNote "C5"
+        SongNote "F#4"
+        SongNote "C5"
+        SongNote "F#4"
         SongNote "B4"
-        SongNote "D#4"
+        SongNote "D4"
+        SongNote "B4"
+        SongNote "D4"
+        SongNote "B4"
+        SongNote "D4"
+        SongNote "B4"
+        SongNote "F4"
+        SongNote "G#4"
+        SongNote "F4"
+        SongNote "C#5"
+        SongNote "F4"
     SongRepeat
     SongRepeatSetup $2
-        SongNote "F#4"
-        SongNote "G4"
+        SongNote "G#4"
         SongNote "A4"
         SongNote "B4"
-        SongNote "D5"
-        SongNote "B4"
-        SongNote "F#5"
-        SongNote "D5"
-        SongNote "A4"
-        SongNote "G4"
-        SongNote "F#4"
-        SongNote "B4"
-        SongNote "A5"
-        SongNote "F#5"
-        SongNote "D5"
-        SongNote "A4"
-        SongNote "G4"
-        SongNote "F#4"
-        SongNote "E4"
-        SongNote "G4"
-        SongNote "A4"
-        SongNote "D5"
+        SongNote "C#5"
         SongNote "E5"
+        SongNote "C#5"
+        SongNote "G#5"
+        SongNote "E5"
+        SongNote "B4"
+        SongNote "A4"
+        SongNote "G#4"
+        SongNote "C#5"
+        SongNote "B5"
+        SongNote "G#5"
+        SongNote "E5"
+        SongNote "B4"
+        SongNote "A4"
+        SongNote "G#4"
+        SongNote "F#4"
+        SongNote "A4"
+        SongNote "B4"
+        SongNote "E5"
+        SongNote "F#5"
         SongRest
     SongRepeat
     SongRepeatSetup $4
         SongNoteLength $1 ;1/8 seconds
+        SongNote "B5"
+        SongRest
         SongNote "A5"
         SongRest
-        SongNote "G5"
+        SongNote "G#5"
         SongRest
         SongNote "F#5"
         SongRest
-        SongNote "E5"
+        SongNote "G#5"
         SongRest
-        SongNote "F#5"
-        SongRest
-        SongNote "G5"
+        SongNote "A5"
         SongRest
     SongRepeat
     SongEnd
@@ -133,20 +133,12 @@ SongKraidSQ2:
 SongKraidTri:
     SongRepeatSetup $10
         SongNoteLength $2 ;1/4 seconds
-        SongNote "E3"
+        SongNote "F#3"
         SongNoteLength $3 ;1/2 seconds
-        SongNote "E4"
+        SongNote "F#4"
     SongRepeat
     SongRepeatSetup $2
         SongNoteLength $2 ;1/4 seconds
-        SongNote "C3"
-        SongNoteLength $3 ;1/2 seconds
-        SongNote "C4"
-        SongNoteLength $2 ;1/4 seconds
-        SongNote "C3"
-        SongNoteLength $3 ;1/2 seconds
-        SongNote "C4"
-        SongNoteLength $2 ;1/4 seconds
         SongNote "D3"
         SongNoteLength $3 ;1/2 seconds
         SongNote "D4"
@@ -154,41 +146,49 @@ SongKraidTri:
         SongNote "D3"
         SongNoteLength $3 ;1/2 seconds
         SongNote "D4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "E3"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "E4"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "E3"
+        SongNoteLength $3 ;1/2 seconds
+        SongNote "E4"
     SongRepeat
     SongRepeatSetup $4
         SongNoteLength $2 ;1/4 seconds
-        SongNote "E3"
-        SongNote "E4"
-        SongNote "E5"
         SongNote "F#3"
         SongNote "F#4"
         SongNote "F#5"
-        SongNote "F3"
-        SongNote "F4"
-        SongNote "F5"
-        SongNote "B2"
-        SongNote "B3"
-        SongNote "B4"
+        SongNote "G#3"
+        SongNote "G#4"
+        SongNote "G#5"
+        SongNote "G3"
+        SongNote "G4"
+        SongNote "G5"
+        SongNote "C#3"
+        SongNote "C#4"
+        SongNote "C#5"
     SongRepeat
     SongRepeatSetup $2
         SongNoteLength $3 ;1/2 seconds
-        SongNote "E3"
+        SongNote "F#3"
         SongNoteLength $2 ;1/4 seconds
-        SongNote "B3"
+        SongNote "C#4"
         SongNoteLength $3 ;1/2 seconds
-        SongNote "C4"
+        SongNote "D4"
         SongNoteLength $2 ;1/4 seconds
-        SongNote "B3"
+        SongNote "C#4"
         SongNoteLength $3 ;1/2 seconds
-        SongNote "C3"
+        SongNote "D3"
         SongNoteLength $2 ;1/4 seconds
-        SongNote "G3"
-        SongNoteLength $3 ;1/2 seconds
         SongNote "A3"
-        SongNoteLength $2 ;1/4 seconds
+        SongNoteLength $3 ;1/2 seconds
         SongNote "B3"
+        SongNoteLength $2 ;1/4 seconds
+        SongNote "C#4"
     SongRepeat
     SongRepeatSetup $8
         SongNoteLength $4 ;1 second
-        SongNote "E2"
+        SongNote "F#2"
     SongRepeat
